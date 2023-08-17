e4fd53ad5f26d606552981200c36765517829cdb
S Chopra <sanjaybenu@gmail.com>
2023-08-17 21:21:12 +1000

    refactored to provide a responsive header and styled switch for theme toggle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import { useState } from "react";
-import { Paper, Typography, Switch, Box } from "@mui/material";
-//import Grid from "@mui/material/Unstable_Grid2";
+import { Paper, Box } from "@mui/material";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import "./App.css";
-import Header from "./components/Header";
 import Page from "./components/Page";
-import Navigation from "./components/Navigation";
-//import { Navigation } from "@mui/icons-material";
-
-// import Avatar from '@mui/material/Avatar';
-// import Stack from '@mui/material/Stack';
-//const avatar = require("./images/avatar.png");
+import Header from "./components/Header";
+import StyledSwitch from "./components/StyledSwitch";
 const darkTheme = createTheme({
   palette: {
     mode: "dark",
-  },
-
-  typography: {
-    h4: {
-      color: "#ffffff", // White text color for dark theme
-    },
   },
 });
 
@@ -27,19 +15,15 @@ const lightTheme = createTheme({
   palette: {
     mode: "light",
   },
-
-  typography: {
-    h4: {
-      color: "#000000", // Black text color for light theme
-    },
-  },
 });
 
 function App() {
-  const [theme, setTheme] = useState(darkTheme);
+  const [theme, setTheme] = useState(lightTheme);
   const [btncolor, setBtncolor] = useState("secondary");
   const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === darkTheme ? lightTheme : darkTheme));
+    setTheme((prevTheme) =>
+      prevTheme === lightTheme ? darkTheme : lightTheme
+    );
     setBtncolor((prevColor) =>
       prevColor === "secondary" ? "default" : "secondary"
     );
@@ -55,71 +39,26 @@ function App() {
   const [currentPage, setCurrentPage] = useState(pages[0]);
   return (
     <ThemeProvider theme={theme}>
-      <Header>
-        <Navigation
-          pages={pages}
-          currentPage={currentPage}
-          setCurrentPage={setCurrentPage}
-        />
-
-        <Typography variant="subtitle2">Toggle Mode</Typography>
-
-        <Switch
+      <Header
+        pages={pages}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      >
+        {/* <Typography variant="subtitle2">Change Theme</Typography> */}
+        <StyledSwitch
           onClick={toggleTheme}
           variant="contained"
           color={btncolor}
-          sx={{ float: "right" }}
         />
       </Header>
 
       <Paper
-        sx={{ height: "100vh", margin: "0 20", padding: "150px" }}
+        sx={{ height: "100vh", margin: "auto", padding: "50px" }}
         elevation={8}
         maxWidth="sm"
       >
-        {/* <>
-          <Typography
-            variant="h4"
-            component="h5"
-            sx={{ textAlign: "center", marginTop: 10, padding: 8 }}
-          >
-            Let us toggle dark/light mode using material ui react
-          </Typography>
-          <Grid container spacing={2} sx={{ margin: "auto" }} maxWidth="md">
-            <Grid xs={3}>
-              <Paper sx={{ height: 200, textAlign: "center" }} elevation={3}>
-                <Typography>Hello I am an avatar</Typography>
-                <img src={avatar} alt="other avatar" style={{ width: "70%" }} />
-              </Paper>
-            </Grid>
-            <Grid xs={3}>
-              <Paper
-                sx={{
-                  height: 200,
-                  textAlign: "center",
-                  backgroundImage: `url(${avatar})`,
-                  backgroundSize: "cover",
-                  backgroundPosition: "center",
-                }}
-                elevation={3}
-              >
-                Hello I am another avatar
-              </Paper>
-            </Grid>
-            <Grid xs={3}>
-              <Paper sx={{ height: 200, textAlign: "center" }} elevation={3}>
-                <Typography variant="h7" component="h7">
-                  Hello I am a paper
-                </Typography>
-              </Paper>
-            </Grid>
-            <Grid xs={3}>
-              <Paper sx={{ height: 200, textAlign: "center" }} elevation={3}>
-                Hello I am a paper
-              </Paper>
-            </Grid>
-          </Grid>
-          </> */}
+        {/* <ResponsiveAppBar/> */}
+
         <Box>
           <Page currentPage={currentPage} />
         </Box>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,134 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import {
+  AppBar,
+  Typography,
+  Container,
+  Toolbar,
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+  Button,
+  Avatar,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import AdbIcon from "@mui/icons-material/Adb";
+const img = require("../images/sanjay.jpg");
+const Header = (props) => {
+  const { pages = [], currentPage, setCurrentPage } = props;
 
-import {AppBar, Typography} from '@mui/material'
+  useEffect(() => {
+    document.title = currentPage.name;
+  }, [currentPage]);
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  //const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-const img = require('../images/sanjay.jpg')
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
 
-
-const Header = (props)=>{
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
   return (
-      <AppBar sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', flexDirection:"row",padding:"15px" }}>
-        
-          <img src={img} alt="myphoto" style={{ height: '100px', width: 
-          '100px', borderRadius: '50%' }} />
-        
-      
-        
-          <Typography variant="h4">My Portfolio</Typography>
-        
-          
-          {props.children}
-      </AppBar>
-  )
-}
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            My Portfolio
+          </Typography>
 
-export default Header
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((Page) => (
+                <MenuItem key={Page.name} onClick={() => setCurrentPage(Page)}>
+                  <Typography textAlign="center">{Page.name}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            My Portfolio
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((Page) => (
+              <Button
+                key={Page.name}
+                onClick={() => setCurrentPage(Page)} //replace with setCurrentPage
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                {Page.name}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Avatar alt="Sanjay" src={img} />
+          </Box>
+        </Toolbar>
+      </Container>
+      {props.children}
+    </AppBar>
+  );
+};
+
+export default Header;
